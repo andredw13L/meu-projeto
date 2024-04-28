8e0499713a71e120c23599fc4a5f15b6e54a622a
andredw13L <andreribeiro13l@gmail.com>
2024-04-28 19:28:05 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+
+
 
 function App() {
+
+  const nome = "Maria"
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloWorld/>
+      <h1>Testando CSS</h1>
+      <SayMyName nome="André"/>
+      <SayMyName nome="Matheus"/>
+      <SayMyName nome={nome}/>
+      <Pessoa 
+        nome="Rodrigo" 
+        idade="28" 
+        profissao="Programador" 
+        foto="https://via.placeholder.com/150"
+      />
     </div>
+
   );
 }
 
