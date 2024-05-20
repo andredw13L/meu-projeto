05bbc16a8a3b9138b56df7a805c054e30472dea5
andredw13L <andreribeiro13l@gmail.com>
2024-05-20 20:44:10 -0300

    PropTypes e DefaultProps

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,28 @@
-function Item({marca, lancamento}) {
+import PropTypes from 'prop-types'
+
+function Item({marca, ano_lancamento}) {
   return (
     <>
-     <li>{marca} - {lancamento}</li>
+     <li>{marca} - {ano_lancamento}</li>
     </>
   )
+}
+
+
+// Definir um tipo que é esperado ser atribuído à props 
+Item.propTypes = {
+  marca: PropTypes.string.isRequired,
+  ano_lancamento: PropTypes.number,
+}
+
+// Se houver uma default Props o isRequired não gerará um aviso
+
+
+
+// Tipo padrão erm caso de ausência
+Item.defaultProps = {
+  marca: 'Faltou a marca',
+  ano_lancamento: 0,
 }
 
 export default Item
--- src/components/List.js
+++ src/components/List.js
@@ -6,9 +6,9 @@ function List() {
         <>
          <h1>Minha Lista</h1>
          <ul>
-            <Item marca="Ferrari" />
-            <Item marca="Ford" />
-            <Item marca="Fiat" />
+            <Item marca="Ferrari" ano_lancamento={1985}/>
+            <Item marca="Ford" ano_lancamento={1964}/>
+            <Item marca="Fiat" ano_lancamento={2014}/>
          </ul>
         </>
     )
