486222af86bed53003875d822b11a8f81bfb81e5
andredw13L <andreribeiro13l@gmail.com>
2024-06-04 22:20:13 -0300

    State Lift

---
+++ src/components/Saudacao.js
@@ -0,0 +1,13 @@
+function Saudacao({nome}) {
+
+    function gerarsaudacao(algumNome) {
+        return `Olá ${algumNome}, tudo bem?`
+    }
+    
+
+    return (
+        <>{nome &&<p>{gerarsaudacao(nome)}</p>}</>
+    )
+}
+
+export default Saudacao
